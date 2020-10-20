873840014a53afcbb9f40ed19e6e2b7ad815a7a1
hitman-stack <45466709+hitman-stack@users.noreply.github.com>
2020-10-21 02:54:45 +0530

    Sample code to give call to external api and print that data in console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await GetData();
+        }
+        public static async Task GetData()
+        {
+            try
+            {
+                string uri = $"https://rest.entitysport.com/v2/matches/?status=2&token=ec471071441bb2ac538a0ff901abd249&per_page=10&&paged=1";
+                Console.WriteLine(uri);
+                var responseBody = await client.GetStringAsync(uri);
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message {0}", e.Message);
+            }
+        }
+    }
+}
